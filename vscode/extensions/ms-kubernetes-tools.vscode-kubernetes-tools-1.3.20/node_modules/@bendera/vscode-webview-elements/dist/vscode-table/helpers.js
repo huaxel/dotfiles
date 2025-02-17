0102c7afe9e05f5feb508f251ef666ef47ddb366
Juan Benjumea <benjumeamoreno@gmail.com>
2025-02-17 17:39:10 +0100

    Your commit message

---
+++ vscode/extensions/ms-kubernetes-tools.vscode-kubernetes-tools-1.3.20/node_modules/@bendera/vscode-webview-elements/dist/vscode-table/helpers.js
@@ -0,0 +1,20 @@
+export const rawValueToPercentage = (raw, base) => {
+    if (typeof raw === 'number' && !Number.isNaN(raw)) {
+        return (raw / base) * 100;
+    }
+    else if (typeof raw === 'string' && /^[0-9.]+$/.test(raw)) {
+        const val = Number(raw);
+        return (val / base) * 100;
+    }
+    else if (typeof raw === 'string' && /^[0-9.]+%$/.test(raw)) {
+        return Number(raw.substring(0, raw.length - 1));
+    }
+    else if (typeof raw === 'string' && /^[0-9.]+px$/.test(raw)) {
+        const val = Number(raw.substring(0, raw.length - 2));
+        return (val / base) * 100;
+    }
+    else {
+        return null;
+    }
+};
+//# sourceMappingURL=helpers.js.map
