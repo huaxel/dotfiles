ed8104a62c50a9e0644589219c437242b68371b7
Juan Benjumea <benjumeamoreno@gmail.com>
2025-03-17 21:56:04 +0100

    .

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,16 @@ return {
     opts = {
       formatters_by_ft = {
         sql = { "sqlfluff" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
       },
     },
   },
