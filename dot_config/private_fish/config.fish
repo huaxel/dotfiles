a054a77c40ec9d05ff677766de7d32a1a180796c
Juan Benjumea <benjumeamoreno@gmail.com>
2026-01-11 19:58:17 +0100

    Simplify config: migrate to fish, remove zsh and link script

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+
+# Added by Antigravity
+fish_add_path /Users/juanbenjumea/.antigravity/antigravity/bin
